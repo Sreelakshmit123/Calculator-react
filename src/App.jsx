845ee7cd5a25dadbac1ca9547db0f9675cea31bf
Sreelakshmit123 <tsreelakshmi88@gmail.com>
2023-12-18 12:08:14 +0530

    CalculatorApp completed

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleClick = (value) => {
+    setInput((prevInput) => prevInput + value);
+  };
+
+  const handleReset = () => {
+    setInput((prevInput) => prevInput.slice(0, -1));
+  };
+
+  const handleCalculate = () => {
+    try {
+      setResult(eval(input).toString());
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+
+  const handleClear = () => {
+    setInput('');
+    setResult('');
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">
+        <input type="text" value={input} readOnly />
+        <div className="result">{result}</div>
+      </div>
+      <div className="buttons">
+        <button onClick={() => handleClick('1')}>1</button>
+        <button onClick={() => handleClick('2')}>2</button>
+        <button onClick={() => handleClick('3')}>3</button>
+        <button onClick={() => handleClick('+')}>+</button>
+        <button onClick={() => handleClick('4')}>4</button>
+        <button onClick={() => handleClick('5')}>5</button>
+        <button onClick={() => handleClick('6')}>6</button>
+        <button onClick={() => handleClick('-')}>-</button>
+        <button onClick={() => handleClick('7')}>7</button>
+        <button onClick={() => handleClick('8')}>8</button>
+        <button onClick={() => handleClick('9')}>9</button>
+        <button onClick={() => handleClick('*')}>*</button>
+        <button onClick={() => handleClick('0')}>0</button>
+        
+        <button onClick={() => handleClick('/')}>/</button>
+        
+        <button onClick={handleClear}>AC</button>
+        <button onClick={handleCalculate}>=</button>
+       
+        <button onClick={() => handleClick('.')}>.</button> 
+        <button onClick={handleReset}><i class="fa-solid fa-delete-left"></i></button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
